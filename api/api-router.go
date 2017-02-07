827a0df237627d320949f508f10a3119284c887d
SmartBro <smartbrostudio@gmail.com>
2017-02-07 21:27:08 +0200

    add routes with mocks

--- api/api-router.go
+++ api/api-router.go
@@ -1,14 +1,61 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
 
-// APIRouter is a router for api
-var APIRouter = http.NewServeMux()
+	"encoding/json"
 
-func init() {
-	APIRouter.HandleFunc("/", handler)
+	"github.com/gorilla/mux"
+)
+
+type Company struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Employees   int32  `json:"employees"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+// APIRouter is a router for api
+var APIRouter = mux.NewRouter().StrictSlash(false)
+
+func init() {
+	APIRouter.HandleFunc("/api/companies", getCompanies).Methods("GET")
+	APIRouter.HandleFunc("/api/companies", addCompany).Methods("POST")
+	APIRouter.HandleFunc("/api/companies/{id}", updateCompany).Methods("PUT")
+	APIRouter.HandleFunc("/api/companies/{id}", removeCompany).Methods("DELETE")
+}
+
+func getCompanies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	company := &Company{"Facebook", "Social Network", 100500}
+	response, err := json.Marshal(company)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func addCompany(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	company := &Company{"Facebook", "Social Network", 100500}
+	response, err := json.Marshal(company)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
+
+func updateCompany(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	company := &Company{"Facebook", "Social Network", 100500}
+	response, err := json.Marshal(company)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(response)
+}
+
+func removeCompany(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNoContent)
 }
--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"net/http"
 
+	"log"
+
 	"github.com/smartbro/web-server-go/api"
 )
 
 func main() {
-	http.ListenAndServe(":8080", api.APIRouter)
+	address := ":8080"
+	server := &http.Server{
+		Addr:    address,
+		Handler: api.APIRouter,
+	}
+	log.Println("Listening " + address)
+	server.ListenAndServe()
 }
